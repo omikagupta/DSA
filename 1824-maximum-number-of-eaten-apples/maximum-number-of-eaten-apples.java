99da378f17ebb811eea9116ad6a31577cf4f807d
Omika Gupta <24bai70535@cuchd.in>
2026-07-26 13:10:50 +0530

    Time: 91 ms (38.92%) | Memory: 48.8 MB (64.87%) - LeetSync

--- 1824-maximum-number-of-eaten-apples/maximum-number-of-eaten-apples.java
+++ 1824-maximum-number-of-eaten-apples/maximum-number-of-eaten-apples.java
@@ -1,37 +1,25 @@
 class Solution {
     public int eatenApples(int[] apples, int[] days) {
-
-        int ans = 0;
-        int day = 0;
-
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
-
-        while (day < apples.length || !pq.isEmpty()) {
-
-          
-            if (day < apples.length && apples[day] > 0) {
-                pq.offer(new int[]{day + days[day], apples[day]});
-            }
-
-            
-            while (!pq.isEmpty() && pq.peek()[0] <= day) {
-                pq.poll();
-            }
-
-           
-            if (!pq.isEmpty()) {
-                int[] curr = pq.poll();
-                curr[1]--;
+        int day=0;
+        int ans=0;
+        int expiry=0;
+        PriorityQueue<int[]>pq=new PriorityQueue<>((a,b)->a[0]-b[0]);
+        while(day<apples.length || !pq.isEmpty()){
+              while(!pq.isEmpty() && pq.peek()[0] <= day){
+    pq.poll();
+}
+               if(day<apples.length && apples[day]>0 ){
+                 expiry=day+days[day];
+                pq.offer(new int[]{expiry,apples[day]}); }
+                if(!pq.isEmpty()){
+                int [] current=pq.poll();
+               current[1]--;
                 ans++;
-
-                if (curr[1] > 0) {
-                    pq.offer(curr);
-                }
-            }
-
-            day++;
+                
+               if(current[1]>0) pq.offer(current);
+                   } day++;
+    }
+       return ans;
         }
 
-        return ans;
     }
-}
