81d4e00227738b17c186dd6dfe07e42832373a7c
Omika Gupta <24bai70535@cuchd.in>
2026-07-28 15:09:31 +0530

    Time: 2 ms (88.86%) | Memory: 47.9 MB (43.16%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,15 +1,10 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int reach = 0;
-
-        for (int i = 0; i < nums.length; i++) {
-
-            if (i > reach)
-                return false;
-
-            reach = Math.max(reach, i + nums[i]);
-        }
-
-        return true;
+       int maxreach=0;
+       for(int i=0;i<nums.length;i++){
+        if(i>maxreach) return false;
+        maxreach=Math.max(maxreach,i+nums[i]);
+       } 
+       return true;
     }
 }
