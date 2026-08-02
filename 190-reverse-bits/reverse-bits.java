aefae3417156bd0d00e0904dbd32d86ca13fcc83
Omika Gupta <24bai70535@cuchd.in>
2026-08-02 13:33:38 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (85.43%) - LeetSync

--- 190-reverse-bits/reverse-bits.java
+++ 190-reverse-bits/reverse-bits.java
@@ -1,5 +1,13 @@
 class Solution {
     public int reverseBits(int n) {
-        return Integer.reverse(n);
+        int ans = 0;
+
+        for (int i = 0; i < 32; i++) {
+            ans <<= 1;
+            ans |= (n & 1);
+            n >>>= 1;
+        }
+
+        return ans;
     }
 }
