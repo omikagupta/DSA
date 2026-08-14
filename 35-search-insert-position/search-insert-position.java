5a4e013bfeed9679b252416dcaf6860c642482a2
Omika Gupta <24bai70535@cuchd.in>
2026-08-14 14:05:11 +0530

    Time: 0 ms (100.00%) | Memory: 44.9 MB (12.33%) - LeetSync

--- 35-search-insert-position/search-insert-position.java
+++ 35-search-insert-position/search-insert-position.java
@@ -1,22 +1,20 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        int left = 0;
-        int right = nums.length - 1;
+        return binary(nums, target, 0, nums.length - 1);
+    }
 
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
+    public int binary(int[] nums, int target, int left, int right) {
+        if (left > right)
+            return left;
 
-            if (nums[mid] == target) {
-                return mid;
-            } 
-            else if (nums[mid] < target) {
-                left = mid + 1;
-            } 
-            else {
-                right = mid - 1;
-            }
-        }
+        int mid = left + (right - left) / 2;
 
-        return left;
+        if (nums[mid] == target)
+            return mid;
+
+        if (nums[mid] < target)
+            return binary(nums, target, mid + 1, right);
+
+        return binary(nums, target, left, mid - 1);
     }
 }
