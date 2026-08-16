8a8ff9d4bb3d916ca6a54df4f9a99ec2ee6d3bb2
Omika Gupta <24bai70535@cuchd.in>
2026-08-16 22:29:15 +0530

    Time: 0 ms (100.00%) | Memory: 47 MB (35.15%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
@@ -15,9 +15,13 @@
  */
 class Solution {
     public int maxDepth(TreeNode root) {
-        if(root==null) return 0;
-        int lh=maxDepth(root.left);
-        int rh=maxDepth(root.right);
-        return 1+Math.max(lh,rh);
+
+        if (root == null)
+            return 0;
+
+        int lh = maxDepth(root.left);
+        int rh = maxDepth(root.right);
+
+        return 1 + Math.max(lh, rh);
     }
 }
