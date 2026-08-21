7531fae4164f9f5b162c1c410e4343893f5f5331
Omika Gupta <24bai70535@cuchd.in>
2026-08-21 11:39:12 +0530

    Time: 183 ms (9.80%) | Memory: 43.4 MB (94.12%) - LeetSync

---
+++ 3375-kth-smallest-amount-with-single-denomination-combination/kth-smallest-amount-with-single-denomination-combination.java
@@ -0,0 +1,64 @@
+class Solution {
+    public long findKthSmallest(int[] coins, int k) {
+        long low = 1;
+        long high = (long) coins[0] * k;
+
+        while (low < high) {
+            long mid = low + (high - low) / 2;
+
+            if (count(mid, coins) >= k) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return low;
+    }
+
+    private long count(long x, int[] coins) {
+        int n = coins.length;
+        long total = 0;
+
+        for (int mask = 1; mask < (1 << n); mask++) {
+            long lcm = 1;
+            boolean valid = true;
+            int bits = 0;
+
+            for (int i = 0; i < n; i++) {
+                if ((mask & (1 << i)) != 0) {
+                    bits++;
+
+                    long g = gcd(lcm, coins[i]);
+                    lcm = lcm / g * coins[i];
+
+                    if (lcm > x) {
+                        valid = false;
+                        break;
+                    }
+                }
+            }
+
+            if (!valid) continue;
+
+            long multiples = x / lcm;
+
+            if (bits % 2 == 1) {
+                total += multiples;
+            } else {
+                total -= multiples;
+            }
+        }
+
+        return total;
+    }
+
+    private long gcd(long a, long b) {
+        while (b != 0) {
+            long temp = a % b;
+            a = b;
+            b = temp;
+        }
+        return a;
+    }
+}
