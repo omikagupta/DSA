c701d09534e56c1fed37844b95d906cdda519153
Omika Gupta <24bai70535@cuchd.in>
2026-08-26 20:40:35 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (82.87%) - LeetSync

---
+++ 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    string shortestBeautifulSubstring(string s, int k) {
+        int left = 0;
+        int ones = 0;
+        string ans = "";
+
+        for (int right = 0; right < s.size(); right++) {
+            if (s[right] == '1') {
+                ones++;
+            }
+
+            while (ones > k) {
+                if (s[left] == '1') {
+                    ones--;
+                }
+                left++;
+            }
+
+            if (ones == k) {
+                // Remove unnecessary leading zeros
+                while (left <= right && s[left] == '0') {
+                    left++;
+                }
+
+                string cur = s.substr(left, right - left + 1);
+
+                if (ans.empty() ||
+                    cur.length() < ans.length() ||
+                    (cur.length() == ans.length() && cur < ans)) {
+                    ans = cur;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
