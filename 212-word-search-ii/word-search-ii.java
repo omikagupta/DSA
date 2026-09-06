70dec56e6fc262fc42a064bda5c67ba7da4eae33
Omika Gupta <24bai70535@cuchd.in>
2026-09-06 16:36:35 +0530

    Time: 153 ms (65.38%) | Memory: 47.2 MB (30.64%) - LeetSync

---
+++ 212-word-search-ii/word-search-ii.java
@@ -0,0 +1,98 @@
+class Solution {
+
+ 
+    class TrieNode {
+        TrieNode[] children = new TrieNode[26];
+        String word;
+    }
+
+    public List<String> findWords(char[][] board, String[] words) {
+
+        List<String> ans = new ArrayList<>();
+
+     
+        TrieNode root = new TrieNode();
+
+        for (String word : words) {
+            TrieNode node = root;
+
+            for (char ch : word.toCharArray()) {
+                int index = ch - 'a';
+
+                if (node.children[index] == null) {
+                    node.children[index] = new TrieNode();
+                }
+
+                node = node.children[index];
+            }
+
+            node.word = word;
+        }
+
+        for (int row = 0; row < board.length; row++) {
+            for (int col = 0; col < board[0].length; col++) {
+
+                backtrack(board, row, col, root, ans);
+            }
+        }
+
+        return ans;
+    }
+
+    void backtrack(
+        char[][] board,
+        int row,
+        int col,
+        TrieNode node,
+        List<String> ans
+    ) {
+
+      
+        if (row < 0 || col < 0 ||
+            row >= board.length ||
+            col >= board[0].length) {
+            return;
+        }
+
+    
+        if (board[row][col] == '#') {
+            return;
+        }
+
+        char ch = board[row][col];
+
+        if (node.children[ch - 'a'] == null) {
+            return;
+        }
+
+
+        node = node.children[ch - 'a'];
+
+        
+        if (node.word != null) {
+            ans.add(node.word);
+
+          
+            node.word = null;
+        }
+
+        
+        board[row][col] = '#';
+
+     
+        backtrack(board, row + 1, col, node, ans);
+
+       
+        backtrack(board, row - 1, col, node, ans);
+
+        
+        backtrack(board, row, col + 1, node, ans);
+
+     
+        backtrack(board, row, col - 1, node, ans);
+
+     
+        board[row][col] = ch;
+    }
+}
+
