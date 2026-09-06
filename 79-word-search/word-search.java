7951ad52dbc94ab5418e99feaa70743d37d8a363
Omika Gupta <24bai70535@cuchd.in>
2026-09-06 15:26:06 +0530

    Time: 144 ms (41.80%) | Memory: 42.8 MB (88.02%) - LeetSync

---
+++ 79-word-search/word-search.java
@@ -0,0 +1,78 @@
+class Solution {
+
+    public boolean exist(char[][] board, String word) {
+
+        boolean[][] visited =
+            new boolean[board.length][board[0].length];
+
+        for (int i = 0; i < board.length; i++) {
+
+            for (int j = 0; j < board[0].length; j++) {
+
+                if (backtrack(board, word, i, j, 0, visited)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public boolean backtrack(
+        char[][] board,
+        String word,
+        int row,
+        int col,
+        int index,
+        boolean[][] visited
+    ) {
+
+        if (index == word.length()) {
+            return true;
+        }
+
+        if (row < 0 || col < 0 ||
+            row >= board.length ||
+            col >= board[0].length) {
+
+            return false;
+        }
+
+      
+        if (visited[row][col]) {
+            return false;
+        }
+
+        if (board[row][col] != word.charAt(index)) {
+            return false;
+        }
+
+
+        visited[row][col] = true;
+
+        
+        if (backtrack(board, word, row + 1, col, index + 1, visited)) {
+            return true;
+        }
+
+       
+        if (backtrack(board, word, row - 1, col, index + 1, visited)) {
+            return true;
+        }
+
+        
+        if (backtrack(board, word, row, col + 1, index + 1, visited)) {
+            return true;
+        }
+
+      
+        if (backtrack(board, word, row, col - 1, index + 1, visited)) {
+            return true;
+        }
+
+        visited[row][col] = false;
+
+        return false;
+    }
+}
+
