a90463d5274a402e945619a7163ded8e10e4a2ad
Omika Gupta <24bai70535@cuchd.in>
2026-09-09 12:04:15 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (96.79%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -15,15 +15,26 @@ class Solution {
 // }
 // dp[n]=helper(n-1,dp)+helper(n-2,dp);
 // return dp[n];
- if(n==1) return 1;
-        if(n==2) return 2;
-              int[]dp=new int[n+1];
-        dp[1]=1;
-        dp[2]=2;
+//  if(n==1) return 1;
+//         if(n==2) return 2;
+//               int[]dp=new int[n+1];
+//         dp[1]=1;
+//         dp[2]=2;
   
-    for(int i=3;i<=n;i++){
-        dp[i]=dp[i-1]+dp[i-2];
-    }
-    return dp[n];
+//     for(int i=3;i<=n;i++){
+//         dp[i]=dp[i-1]+dp[i-2];
+//     }
+//     return dp[n];
+if(n==1) return 1;
+if(n==2) return 2;
+int prev1=2;
+int prev2=1;
+for(int i=3;i<=n;i++){
+    int current=prev2+prev1;
+    prev2=prev1;
+    prev1=current;
+
+}
+return prev1;
     }
 }
