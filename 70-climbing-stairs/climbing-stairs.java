19fc0c6b659bef1f7f022dd361a1a5eb6cf058db
Omika Gupta <24bai70535@cuchd.in>
2026-09-09 11:47:57 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (17.32%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -3,17 +3,27 @@ class Solution {
         // if(n==1) return 1;
         // if(n==2) return 2;
         // return climbStairs(n-1)+climbStairs(n-2); TLE
-        int[]dp=new int[n+1];
-        return helper(n,dp);
+//         int[]dp=new int[n+1];
+//         return helper(n,dp);
 
-    }
-    public int helper(int n,int[]dp){
-        if(n==1) return 1;
+//     }
+//     public int helper(int n,int[]dp){
+//         if(n==1) return 1;
+//         if(n==2) return 2;
+// if(dp[n]!=0){
+//     return dp[n];
+// }
+// dp[n]=helper(n-1,dp)+helper(n-2,dp);
+// return dp[n];
+ if(n==1) return 1;
         if(n==2) return 2;
-if(dp[n]!=0){
+              int[]dp=new int[n+1];
+        dp[1]=1;
+        dp[2]=2;
+  
+    for(int i=3;i<=n;i++){
+        dp[i]=dp[i-1]+dp[i-2];
+    }
     return dp[n];
-}
-dp[n]=helper(n-1,dp)+helper(n-2,dp);
-return dp[n];
     }
 }
