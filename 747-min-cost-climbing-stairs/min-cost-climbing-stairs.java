c86e2f94e367f70c3352cbb512e5d2b9075bdd8c
Omika Gupta <24bai70535@cuchd.in>
2026-09-09 15:48:47 +0530

    Time: 0 ms (100.00%) | Memory: 45.1 MB (9.70%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
@@ -21,26 +21,35 @@ class Solution {
         int[] dp = new int[n];
         Arrays.fill(dp, -1);
 
-        return Math.min(
-            helper(cost, n - 1, dp),
-            helper(cost, n - 2, dp)
-        );
+    //     return Math.min(
+    //         helper(cost, n - 1, dp),
+    //         helper(cost, n - 2, dp)
+    //     );
+    // }
+
+    // int helper(int[] cost, int i, int[] dp) {
+    //     if (i == 0) return cost[0];
+    //     if (i == 1) return cost[1];
+
+    //     if (dp[i] != -1) {
+    //         return dp[i];
+    //     }
+
+    //     dp[i] = cost[i] + Math.min(
+    //         helper(cost, i - 1, dp),
+    //         helper(cost, i - 2, dp)
+    //     );
+
+    //     return dp[i];
+    dp[0]=cost[0];
+    dp[1]=cost[1];
+    for(int i=2;i<n;i++){
+dp[i]=cost[i]+Math.min(
+    dp[i-1],dp[i-2]
+);
+
     }
-
-    int helper(int[] cost, int i, int[] dp) {
-        if (i == 0) return cost[0];
-        if (i == 1) return cost[1];
-
-        if (dp[i] != -1) {
-            return dp[i];
-        }
-
-        dp[i] = cost[i] + Math.min(
-            helper(cost, i - 1, dp),
-            helper(cost, i - 2, dp)
-        );
-
-        return dp[i];
+    return Math.min(dp[n - 1], dp[n - 2]);
     
 
     }
