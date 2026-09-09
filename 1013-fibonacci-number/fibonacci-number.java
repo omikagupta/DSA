89b66a9c74a6986c10bdd110c6286c1d7f1dc405
Omika Gupta <24bai70535@cuchd.in>
2026-09-09 10:54:21 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (15.94%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -5,11 +5,8 @@ class Solution {
        int prev2=0;
        for(int i=2;i<=n;i++){
         int current=prev1+prev2;
-       
         prev2=prev1;
         prev1=current;
-        
-
        }
        return prev1;
     }
