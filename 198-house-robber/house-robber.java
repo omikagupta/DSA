e56a57ca531a3a48838d3be54a994ce9e67eff4a
Omika Gupta <24bai70535@cuchd.in>
2026-09-10 22:13:02 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (19.36%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -3,24 +3,33 @@ class Solution {
         int n = nums.length;
 
         int[] dp = new int[n];
-        Arrays.fill(dp, -1);
+        // Arrays.fill(dp, -1);
 
-        return helper(nums, dp, n - 1);
+    //     return helper(nums, dp, n - 1);
+    // }
+
+    // public int helper(int[] nums, int[] dp, int i) {
+//    if (i == 0) return nums[0];
+//    if (i == 1) return Math.max(nums[0], nums[1]);
+
+    //     if (dp[i] != -1) {
+    //         return dp[i];
+    //     }
+
+    //     dp[i] = Math.max(
+    //         helper(nums, dp, i - 1),
+    //         helper(nums, dp, i - 2) + nums[i]
+    //     );
+
+    //     return dp[i];
+    if (nums.length == 1) return nums[0];
+    if (nums.length == 2) return Math.max(nums[0],nums[1]);
+dp[0]=nums[0];
+dp[1]=Math.max(nums[0],nums[1]);
+
+    for( int i=2;i<n;i++){
+dp[i]=Math.max(dp[i-1],dp[i-2]+nums[i]);
     }
-
-    public int helper(int[] nums, int[] dp, int i) {
-        if (i == 0) return nums[0];
-        if (i == 1) return Math.max(nums[0], nums[1]);
-
-        if (dp[i] != -1) {
-            return dp[i];
-        }
-
-        dp[i] = Math.max(
-            helper(nums, dp, i - 1),
-            helper(nums, dp, i - 2) + nums[i]
-        );
-
-        return dp[i];
+    return dp[n-1];
     }
 }
