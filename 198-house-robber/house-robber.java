149041ab2dbafc5b4088fd5213102cd9a45ac992
Omika Gupta <24bai70535@cuchd.in>
2026-09-10 22:21:44 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (56.41%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -2,7 +2,7 @@ class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
 
-        int[] dp = new int[n];
+        // int[] dp = new int[n];
         // Arrays.fill(dp, -1);
 
     //     return helper(nums, dp, n - 1);
@@ -22,14 +22,26 @@ class Solution {
     //     );
 
     //     return dp[i];
-    if (nums.length == 1) return nums[0];
-    if (nums.length == 2) return Math.max(nums[0],nums[1]);
-dp[0]=nums[0];
-dp[1]=Math.max(nums[0],nums[1]);
+//     if (nums.length == 1) return nums[0];
+//     if (nums.length == 2) return Math.max(nums[0],nums[1]);
+// dp[0]=nums[0];
+// dp[1]=Math.max(nums[0],nums[1]);
 
-    for( int i=2;i<n;i++){
-dp[i]=Math.max(dp[i-1],dp[i-2]+nums[i]);
+//     for( int i=2;i<n;i++){
+// dp[i]=Math.max(dp[i-1],dp[i-2]+nums[i]);
+//     }
+//     return dp[n-1];
+//     }
+ if (nums.length == 1) return nums[0];
+  if (nums.length == 2) return Math.max(nums[0],nums[1]);
+int prev2 = nums[0];
+int prev1 = Math.max(nums[0], nums[1]);
+for(int i=2;i<n;i++){
+    int current=Math.max(prev1,nums[i]+prev2);
+    prev2=prev1;
+    prev1=current;
+}
+return prev1;
     }
-    return dp[n-1];
-    }
+
 }
