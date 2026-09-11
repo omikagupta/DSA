b573c4d2bedf349c8dd4f641326d6b75a0ce3d8c
Omika Gupta <24bai70535@cuchd.in>
2026-09-11 11:50:52 +0530

    Time: 2 ms (99.87%) | Memory: 47.1 MB (34.94%) - LeetSync

---
+++ 740-delete-and-earn/delete-and-earn.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int deleteAndEarn(int[] nums) {
+
+        int max = 0;
+
+        for (int num : nums) {
+            max = Math.max(max, num);
+        }
+
+     
+        int[] points = new int[max + 1];
+
+        for (int num : nums) {
+            points[num] += num;
+        }
+
+        int[] dp = new int[max + 1];
+
+        dp[0] = 0;
+
+        if (max >= 1) {
+            dp[1] = points[1];
+        }
+
+        for (int i = 2; i <= max; i++) {
+
+       
+            int skip = dp[i - 1];
+
+            int take = points[i] + dp[i - 2];
+
+            dp[i] = Math.max(skip, take);
+        }
+
+        return dp[max];
+    }
+}
